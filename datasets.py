7524a9c87b77e9d4a0b3bc1662cb79e60f5cbade
hclimente <hclimente@gmail.com>
2021-02-08 10:06:39 +0100

    first implementation

---
+++ datasets.py
@@ -0,0 +1,49 @@
+import random
+
+from torch.utils.data import Dataset
+
+
+class MultiCellDataset (Dataset):
+
+    def __init__(self, dataset_1, dataset_2, transform=None):
+        self.dataset_1 = dataset_1
+        self.dataset_2 = dataset_2
+        self.transform = transform
+
+    def __getitem__(self, index):
+        random.seed(index)
+        cell1, moa1 = random.choice(self.dataset_1)
+
+        same_moa = random.getrandbits(1)
+
+        while True:
+            cell2, moa2 = random.choice(self.dataset_2)
+
+            if same_moa and moa1 == moa2:
+                break
+            elif not same_moa and moa1 != moa2:
+                break
+
+        if self.transform:
+            cell1 = self.transform(cell1)
+            cell2 = self.transform(cell2)
+
+        return cell1, cell2, same_moa
+
+    def __len__(self):
+        return int(len(self.dataset_1) * len(self.dataset_2) / 2)
+
+
+class Boyd2019(MultiCellDataset):
+
+    def __init__(self, path_mda231, path_mda468):
+
+        window_width = 32
+
+        dataset_1 = self.load_dataset(path_mda231)
+        dataset_2 = self.load_dataset(path_mda468)
+
+        super().__init__(dataset_1, dataset_2)
+
+    def load_dataset(self, path):
+        return path
---
+++ networks.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+
+
+class SiameseNet(nn.Module):
+
+    def __init__(self):
+        super(SiameseNet, self).__init__()
+
+        self.convnet = nn.Sequential(nn.Conv2d(3, 32, 5),
+                                     nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2),
+                                     nn.Conv2d(32, 64, 5),
+                                     nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2))
+
+        self.fc = nn.Sequential(nn.Linear(692224, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 2))
+
+    def forward(self, x1, x2):
+        output1 = self.forward_one(x1)
+        output2 = self.forward_one(x2)
+        return output1, output2
+
+    def forward_one(self, x):
+        output = self.convnet(x)
+        output = output.view(1, -1)
+        output = self.fc(output)
+        return output
