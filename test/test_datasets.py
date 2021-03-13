fixed mean computation tests

test/test_datasets.py
@@ -5,14 +5,19 @@ from datasets import Boyd2019
 
 def test_read_metadata():
 
-    df = Boyd2019.read_metadata('../multi-cell-line/PlateMap-KPP_MOA.xlsx')
+    df = Boyd2019.read_metadata('../data/boyd_2019_PlateMap-KPP_MOA.xlsx')
 
     assert df.shape == (384, 9)
 
+
 def test_get_normalization_params():
-    a = torch.randn(4, 4, 10)
+    row = torch.linspace(0, 4, 5)
+    channel = torch.vstack([row, row, row, row, row])
+    img = torch.stack([channel, channel, channel])
 
-    Boyd2019.get_normalization_params(a)
+    mean, std = Boyd2019.get_normalization_params(img)
 
-    Boyd2019('../multi-cell-line/PlateMap-KPP_MOA.xlsx')
-    assert a.shape == (4, 4)
+    assert mean.shape == (5, 5)
+    assert std.shape == (5, 5)
+    assert torch.all(mean == channel)
+    assert torch.all(std == 0)