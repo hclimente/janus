75440cc1f3086aa5adeefbc92e4925e16d56ccad
hclimente <hclimente@gmail.com>
2021-03-15 12:04:58 +0100

    added parameter loader

--- datasets.py
+++ datasets.py
@@ -1,7 +1,9 @@
 from functools import partial
 import random
 
+from os.path import join
 import pandas as pd
+import pickle
 from torchvision import transforms
 import torch
 from torch.utils.data import Dataset
@@ -56,35 +58,28 @@ class MultiCellDataset (Dataset):
 
 class Boyd2019(MultiCellDataset):
 
-    def __init__(self, path_metadata,
+    def __init__(self, data_path, metadata,
                  transform=transforms.Compose([transforms.RandomHorizontalFlip(),
                                                transforms.RandomVerticalFlip(),
                                                RandomRot90()])):
 
         padding = 32
-        self.metadata = self.read_metadata(path_metadata)
-
-        self.avg_mda231 = None
-        self.std_mda231 = None
-        self.avg_mda468 = None
-        self.std_mda468 = None
 
         # wrap iterators in partial to get a fresh call
-        mda231_crops = HDF5Reader.get_crops('test/data/22_384_20X-hNA_D_F_C3_C5_20160031_2016.01.25.17.23.13_MDA231',
-                                            self.metadata, padding, shuffle=True)
+        mda231_crops = HDF5Reader.get_crops(join(data_path,
+                                                 '22_384_20X-hNA_D_F_C3_C5_20160031_2016.01.25.17.23.13_MDA231'),
+                                            metadata, padding)
         self.mda231 = torch.stack([x for x, _ in mda231_crops])
 
-        mda468_crops = HDF5Reader.get_crops('test/data/22_384_20X-hNA_D_F_C3_C5_20160032_2016.01.25.16.27.22_MDA468',
-                                            self.metadata, padding, shuffle=True)
+        mda468_crops = HDF5Reader.get_crops(join(data_path,
+                                                 '22_384_20X-hNA_D_F_C3_C5_20160032_2016.01.25.16.27.22_MDA468'),
+                                            metadata, padding)
         self.mda468 = torch.stack([x for x, _ in mda468_crops])
 
-        if not self.avg_mda231 or not self.std_mda231:
-            self.avg_mda231, self.std_mda231 = self.get_normalization_params(self.mda231)
+        self.avg_mda231, self.std_mda231 = self.load_parameters(join(data_path, 'mda231_params.pkl'), self.mda231)
+        self.avg_mda468, self.std_mda468 = self.load_parameters(join(data_path, 'mda468_params.pkl'), self.mda468)
 
-        if not self.avg_mda468 or not self.std_mda468:
-            self.avg_mda468, self.std_mda468 = self.get_normalization_params(self.mda468)
-
-        super().__init__(self.mda231, self.mda468, self.metadata, transform)
+        super().__init__(self.mda231, self.mda468, metadata, transform)
 
     @staticmethod
     def read_metadata(metadata_path):
@@ -102,5 +97,18 @@ class Boyd2019(MultiCellDataset):
 
         avg = torch.mean(imgs, dim=0)
         std = torch.std(imgs, dim=0)
+
+        return avg, std
+
+    @staticmethod
+    def load_parameters(pickle_path, crops):
+
+        try:
+            with open(pickle_path, 'rb') as parameters:
+                avg, std = pickle.load(parameters)
+        except IOError:
+            with open(pickle_path, 'wb') as parameters:
+                avg, std = Boyd2019.get_normalization_params(crops)
+                pickle.dump((avg, std), parameters)
 
         return avg, std
--- test/test_datasets.py
+++ test/test_datasets.py
@@ -1,4 +1,4 @@
-import numpy as np
+import os
 import pandas as pd
 import torch
 
@@ -8,17 +8,33 @@ from readers import HDF5Reader
 
 def test_boyd2019():
 
-    d = Boyd2019('data/boyd_2019_PlateMap-KPP_MOA.xlsx')
+    assert not os.path.isfile('test/data/mda231_params.pkl')
+    assert not os.path.isfile('test/data/mda468_params.pkl')
+
+    metadata = pd.DataFrame({'well': ['A01', 'B02']})
+    d = Boyd2019('test/data', metadata)
 
     assert torch.all(d.mda231 == d.dataset_1)
-    assert d.mda231.shape[1:] == (3, 64, 64)
+    assert d.mda231.shape == (891, 3, 64, 64)
     assert d.avg_mda231.shape == (3, 64, 64)
     assert d.std_mda231.shape == (3, 64, 64)
+    assert os.path.isfile('test/data/mda231_params.pkl')
 
     assert torch.all(d.mda468 == d.dataset_2)
-    assert d.mda468.shape[1:] == (3, 64, 64)
+    assert d.mda468.shape == (609, 3, 64, 64)
     assert d.avg_mda468.shape == (3, 64, 64)
     assert d.std_mda468.shape == (3, 64, 64)
+    assert os.path.isfile('test/data/mda468_params.pkl')
+
+    # test parameter retrieval
+    new = Boyd2019('test/data', metadata)
+    assert torch.all(new.avg_mda231 == d.avg_mda231)
+    assert torch.all(new.std_mda231 == d.std_mda231)
+    assert torch.all(new.avg_mda468 == d.avg_mda468)
+    assert torch.all(new.std_mda468 == d.std_mda468)
+
+    os.remove('test/data/mda231_params.pkl')
+    os.remove('test/data/mda468_params.pkl')
 
 
 def test_read_metadata():
@@ -59,3 +75,27 @@ def test_get_normalization_params():
     assert type(std) is torch.Tensor
     assert std.shape == (3, 64, 64)
 
+
+def test_load_parameters():
+
+    assert not os.path.isfile('test/params.pkl')
+
+    row = torch.linspace(0, 4, 5)
+    crops = row.repeat(4, 3, 5, 1)
+    avg, std = Boyd2019.load_parameters('test/params.pkl', crops)
+
+    assert os.path.isfile('test/params.pkl')
+    assert type(avg) is torch.Tensor
+    assert avg.shape == (3, 5, 5)
+    assert torch.all(avg == row.repeat(5,1))
+
+    assert type(std) is torch.Tensor
+    assert std.shape == (3, 5, 5)
+    assert torch.all(std == 0)
+
+    new_avg, new_std = Boyd2019.load_parameters('test/params.pkl', None)
+
+    assert torch.all(avg == new_avg)
+    assert torch.all(std == new_std)
+
+    os.remove('test/params.pkl')
