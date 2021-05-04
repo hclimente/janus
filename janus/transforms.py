ecfec98bef329cc8f77d5848251e010c70531c9d
hclimente <hclimente@gmail.com>
2021-05-04 11:48:31 +0200

    added extra transforms

--- janus/transforms.py
+++ janus/transforms.py
@@ -1,4 +1,5 @@
 from numpy import random
+import numpy as np
 import torch
 
 
@@ -9,3 +10,28 @@ class RandomRot90(object):
 
     def __call__(self, img):
         return torch.rot90(img, k=random.randint(0, 3), dims=(1, 2))
+
+
+class Normalize(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self, img):
+
+        max = torch.amax(img, dim=(1, 2)).reshape(3, 1, 1) # torch.quantile(channel, 0.999)
+        min = torch.amin(img, dim=(1, 2)).reshape(3, 1, 1)
+        # clip image
+        img = torch.max(torch.min(img, max), min)
+        img = (img - min) / (max - min)
+
+        return img
+
+
+class RGB(object):
+
+    def __init__(self):
+        pass
+
+    def __call__(self, img):
+        return torch.stack((img[1, ...], img[2, ...], img[0, ...]), dim=2)
--- test/test_transforms.py
+++ test/test_transforms.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-from janus.transforms import RandomRot90
+from janus.transforms import Normalize, RandomRot90, RGB
 
 
 def test_random_rot90():
@@ -17,3 +17,34 @@ def test_random_rot90():
         for i in imgs:
             r = rot90(i)
             assert sum([torch.all(r == x) for x in imgs])
+
+
+def test_normalize():
+
+    ch1 = torch.linspace(0, 4, 5).repeat(5, 1)
+    ch2 = torch.linspace(40, 10, 5).repeat(5, 1)
+    ch3 = torch.linspace(100, 400, 5).repeat(5, 1)
+    img = torch.stack((ch1, ch2, ch3))
+
+    norm = Normalize()
+    norm_img = norm(img)
+
+    ch1_n = torch.linspace(0, 1, 5).repeat(5, 1)
+    ch2_n = torch.linspace(1, 0, 5).repeat(5, 1)
+    good_img = torch.stack((ch1_n, ch2_n, ch1_n))
+
+    assert torch.all(good_img == norm_img)
+
+def test_rgb():
+
+    ch1 = torch.linspace(0, 4, 5).repeat(5, 1)
+    ch2 = torch.linspace(40, 10, 5).repeat(5, 1)
+    ch3 = torch.linspace(100, 400, 5).repeat(5, 1)
+    img = torch.stack((ch1, ch2, ch3))
+    good_img = torch.stack((ch2, ch3, ch1))
+    good_img = good_img.permute(1, 2, 0)
+
+    rgb = RGB()
+    img = rgb(img)
+
+    assert torch.all(img == good_img)
