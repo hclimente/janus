f9055dbbddf7994a9f83fbebcbd1c6cbc94f0387
hclimente <hclimente@gmail.com>
2021-05-04 15:52:35 +0200

    added support for pretrained network

--- janus/networks.py
+++ janus/networks.py
@@ -3,26 +3,39 @@ import torch.nn as nn
 
 class SiameseNet(nn.Module):
 
-    def __init__(self, p_dropout=0):
+    def __init__(self, p_dropout=0, embedding_dim=256, feature_extractor=None, idx_cutoff=19):
         super(SiameseNet, self).__init__()
 
-        self.convnet = nn.Sequential(nn.Conv2d(3, 32, 3, padding=1),
-                                     nn.BatchNorm2d(32),
-                                     nn.PReLU(),
-                                     nn.MaxPool2d(2, stride=2),
-                                     nn.Conv2d(32, 64, 3, padding=1),
-                                     nn.BatchNorm2d(64),
-                                     nn.PReLU(),
-                                     nn.MaxPool2d(2, stride=2),
-                                     nn.Conv2d(64, 64, 3, padding=1),
-                                     nn.BatchNorm2d(64),
-                                     nn.PReLU(),
-                                     nn.MaxPool2d(2, stride=2))
+        if feature_extractor:
+            self.convnet = nn.Sequential(*list(feature_extractor.features)[:idx_cutoff],
+                                         nn.Conv2d(256, 512, kernel_size=(3, 3), padding=(1, 1)),
+                                         nn.BatchNorm2d(512),
+                                         nn.PReLU(),
+                                         nn.MaxPool2d(2, stride=2))
+
+            # freeze weights
+            for name, p in self.convnet.named_parameters():
+                idx_param = int(name.split('.')[0])
+                if idx_param <= idx_cutoff:
+                    p.requires_grad = False
+        else:
+            self.convnet = nn.Sequential(nn.Conv2d(3, 32, kernel_size=(3, 3), padding=(1, 1)),
+                                         nn.BatchNorm2d(32),
+                                         nn.PReLU(),
+                                         nn.MaxPool2d(2, stride=2),
+                                         nn.Conv2d(32, 64, kernel_size=(3, 3), padding=(1, 1)),
+                                         nn.BatchNorm2d(64),
+                                         nn.PReLU(),
+                                         nn.MaxPool2d(2, stride=2),
+                                         nn.Conv2d(64, 64, kernel_size=(3, 3), padding=(1, 1)),
+                                         nn.BatchNorm2d(64),
+                                         nn.PReLU(),
+                                         nn.MaxPool2d(2, stride=2))
 
         self.fc = nn.Sequential(nn.Dropout(p_dropout),
                                 nn.Linear(64 * 8 * 8, 256),
                                 nn.PReLU(),
-                                nn.Linear(256, 256))
+                                nn.Linear(256, embedding_dim))
 
     def forward(self, x1, x2):
         output1 = self.embedding(x1)
--- test/test_networks.py
+++ test/test_networks.py
@@ -1,30 +1,43 @@
 import torch
+import torchvision.models as models
 
 from janus.networks import SiameseNet
+
+img1 = torch.rand((1, 3, 64, 64))
+img2 = torch.rand((1, 3, 64, 64))
 
 
 def test_forward():
 
-    img1 = torch.rand((1, 3, 64, 64))
-    img2 = torch.rand((1, 3, 64, 64))
     net = SiameseNet()
 
     output1, output2 = net(img1, img2)
 
     assert type(output1) is torch.Tensor
-    assert output1.shape == (1,256)
+    assert output1.shape == (1, 256)
 
     assert type(output2) is torch.Tensor
-    assert output2.shape == (1,256)
+    assert output2.shape == (1, 256)
 
 
 def test_embedding():
 
-    img1 = torch.rand((1, 3, 64, 64))
-    img2 = torch.rand((1, 3, 64, 64))
-    net = SiameseNet()
+    net = SiameseNet(embedding_dim=512)
+
+    output1, output2 = net(img1, img2)
+
+    assert output1.shape == (1, 512)
+    assert torch.all(output1 == net.embedding(img1))
+    assert torch.all(output2 == net.embedding(img2))
+
+
+def test_vgg():
+
+    vgg19 = models.vgg19(pretrained=True)
+    net = SiameseNet(feature_extractor=vgg19)
 
     output1, output2 = net(img1, img2)
 
     assert torch.all(output1 == net.embedding(img1))
     assert torch.all(output2 == net.embedding(img2))
+
